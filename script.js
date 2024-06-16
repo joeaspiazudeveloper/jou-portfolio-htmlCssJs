811878b5a49b14d1244baaabe6bb53c672aa3f2c
jouaspiazu <joeaspiazu@hotmail.com>
2024-06-15 21:11:05 -0500

    Added BEM css methodology and fixed some issues in mobile and web view

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
+  const menu = document.querySelector(".hamburger-nav__links");
+  const icon = document.querySelector(".hamburger-nav__icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
