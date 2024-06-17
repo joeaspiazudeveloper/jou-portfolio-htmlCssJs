6ff7eafc1aec40bcab22d5619bfe999c47eb6435
jouaspiazu <joeaspiazu@hotmail.com>
2024-06-17 18:36:49 -0500

    Solved issue detected of toggle functionality in mobile

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ if (isDarkMode) {
 }
 
 const toggle = document.getElementById('toggle');
+const toggle1 = document.getElementById('toggle1');
 const body = document.getElementsByClassName('dark')[0];
+
 toggle.onclick = function() {
   toggle.classList.toggle('active');
   body.classList.toggle('dark');
 };
+
+toggle1.onclick = function() {
+  toggle1.classList.toggle('active');
+  body.classList.toggle('dark');
+};
+
 
 function toggleMenu() {
   const menu = document.querySelector(".hamburger-nav__links");
