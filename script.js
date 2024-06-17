ee81509bf243cfdbe0bba944cc3840f4ddff8dae
jouaspiazu <joeaspiazu@hotmail.com>
2024-06-17 18:07:31 -0500

    Added toggle button and functionality to toggle dark and light theme

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ if (isDarkMode) {
   document.body.className = "light";
 }
 
+const toggle = document.getElementById('toggle');
+const body = document.getElementsByClassName('dark')[0];
+toggle.onclick = function() {
+  toggle.classList.toggle('active');
+  body.classList.toggle('dark');
+};
+
 function toggleMenu() {
   const menu = document.querySelector(".hamburger-nav__links");
   const icon = document.querySelector(".hamburger-nav__icon");
